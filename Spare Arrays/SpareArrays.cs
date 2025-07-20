74bcae8f79c2e3cde5d6ac0a588e0671567c2950
Marco Tharwat <markotharwat11@gmail.com>
2025-07-20 23:06:25 +0300

    Create SpareArrays.cs

---
+++ Spare Arrays/SpareArrays.cs
@@ -0,0 +1,82 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    {
+        Dictionary <string,int> stringsDictionary = new ();
+        foreach (string s in strings) {
+
+            if (stringsDictionary.ContainsKey(s))
+                stringsDictionary[s]++;
+            else 
+                stringsDictionary[s] = 1;
+        }
+        List<int> result = new();
+        foreach (string q in queries) {
+            if (stringsDictionary.ContainsKey(q))
+                result.Add(stringsDictionary[q]);
+            else
+                result.Add(0);
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> strings = new List<string>();
+
+        for (int i = 0; i < stringsCount; i++)
+        {
+            string stringsItem = Console.ReadLine();
+            strings.Add(stringsItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(strings, queries);
+
+        textWriter.WriteLine(String.Join("\n", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
